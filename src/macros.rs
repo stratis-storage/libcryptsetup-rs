9a818c6946a702361d4c26d38b67bce499d842f8
John Baublitz <jbaublitz@redhat.com>
2020-01-09 10:46:41 -0500

    Add license headers

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 /// Convert an errno-zero-success return pattern into a `Result<(), LibcryptErr>`
 macro_rules! errno {
     ( $rc:expr ) => {
