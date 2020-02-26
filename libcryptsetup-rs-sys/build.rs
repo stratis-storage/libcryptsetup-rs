35152ee5d266b4338663f625b64dd3d796b4264f
John Baublitz <jbaublitz@redhat.com>
2020-02-26 10:54:10 -0500

    Feature flag crypt_safe_free implementation for 2.2

--- libcryptsetup-rs-sys/build.rs
+++ libcryptsetup-rs-sys/build.rs
@@ -2,18 +2,36 @@ use std::env;
 
 use bindgen;
 use cc;
+use pkg_config::Config;
 
 use std::path::PathBuf;
 
-fn main() {
+fn safe_free_is_needed() -> bool {
+    match Config::new().atleast_version("2.3.0").probe("libcryptsetup") {
+        Ok(_) => false,
+        Err(_) => {
+            match Config::new().atleast_version("2.2.0").probe("libcryptsetup") {
+                Ok(_) => true,
+                Err(e) => panic!("Bindings require at least cryptsetup-2.2: {}", e),
+            }
+        }
+    }
+}
+
+fn build_safe_free() {
     println!("cargo:rustc-link-lib=cryptsetup");
 
     cc::Build::new().file("safe_free.c").compile("safe_free");
+}
 
-    let bindings = bindgen::Builder::default()
-        .header("header.h")
-        .header("safe_free.h")
-        .size_t_is_usize(true)
+fn generate_bindings(safe_free_is_needed: bool) {
+    let builder = bindgen::Builder::default().header("header.h").size_t_is_usize(true);
+    let builder_with_safe_free = if safe_free_is_needed {
+        builder.header("safe_free.h")
+    } else {
+        builder
+    };
+    let bindings = builder_with_safe_free
         .generate()
         .expect("Unable to generate bindings");
 
@@ -21,4 +39,12 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings");
+}
+
+fn main() {
+    let safe_free_is_needed = safe_free_is_needed();
+    if safe_free_is_needed {
+        build_safe_free();
+    }
+    generate_bindings(safe_free_is_needed);
 }
