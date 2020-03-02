f30b12e7b793c2cddd951deb1946dc3d41c41cd5
John Baublitz <jbaublitz@redhat.com>
2020-03-02 11:50:43 -0500

    Code cleanup

--- src/mem.rs
+++ src/mem.rs
@@ -9,8 +9,8 @@ use libc::c_void;
 #[cfg(cryptsetup23supported)]
 use crate::Result;
 
-macro_rules! memzero {
-    ($(#[$docs:meta])* $name:ident, $drop:expr, $(#[$from_ptr_docs:meta])*) => {
+macro_rules! define_handle {
+    ($(#[$docs:meta])* $name:ident, $(#[$from_ptr_docs:meta])* from_ptr $(, $drop:expr)?) => {
         $(#[$docs])*
         #[cfg(cryptsetup23supported)]
         pub struct $name(*mut c_void, usize);
@@ -26,10 +26,27 @@ macro_rules! memzero {
         #[cfg(cryptsetup23supported)]
         impl Drop for $name {
             fn drop(&mut self) {
-                unsafe { $drop(self) }
+                self.safe_memzero();
+                $(
+                    unsafe { $drop(self) };
+                )?
             }
         }
+    };
+}
 
+macro_rules! memzero {
+    ($name:ident) => {
+        impl SafeMemzero for $name {
+            fn safe_memzero(&mut self) {
+                unsafe { libcryptsetup_rs_sys::crypt_safe_memzero(self.0, self.1) }
+            }
+        }
+    };
+}
+
+macro_rules! as_ref {
+    ($name:ident) => {
         impl AsRef<[u8]> for $name {
             fn as_ref(&self) -> &[u8] {
                 unsafe { slice::from_raw_parts(self.0 as *const _ as *const u8, self.1) }
@@ -44,7 +61,16 @@ macro_rules! memzero {
     };
 }
 
-memzero ! {
+#[cfg(cryptsetup23supported)]
+pub trait SafeMemzero {
+    /// Zero the data in the buffer. This is not necessary in most circumstances
+    /// unless the user wants to reuse the buffer and reinitialize with zeros.
+    /// This method is never needed before the value is dropped as `Drop` will
+    /// safely zero the memory for the user.
+    fn safe_memzero(&mut self);
+}
+
+define_handle! {
     /// Handle for zeroing owned memory. "Owned" in this context refers to memory
     /// that has been allocated and stored in some kind of `char **` argument
     /// in the context of C FFI. This means that the memory has been allocated
@@ -57,10 +83,6 @@ memzero ! {
     /// or any other function compatible with `libc::free`. If it has not been,
     /// you could cause memory corruption and security problems.
     SafeOwnedMemZero,
-    |self_: &mut SafeOwnedMemZero| {
-        libcryptsetup_rs_sys::crypt_safe_memzero(self_.0, self_.1);
-        libc::free(self_.0);
-    },
     /// Construct a safe memory handle from a pointer and a size.
     ///
     /// # Safety
@@ -69,25 +91,33 @@ memzero ! {
     /// compatible with `libc::free`. See the struct-level security warning for more
     /// information. The `size` argument also must match the length of the
     /// allocated block or memory corruption could occur.
+    from_ptr,
+    |self_: &mut SafeOwnedMemZero| {
+        libc::free(self_.0);
+    }
 }
+#[cfg(cryptsetup23supported)]
+memzero!(SafeOwnedMemZero);
+as_ref!(SafeOwnedMemZero);
 
-memzero ! {
+define_handle! {
     /// Handle for zeroing borrowed memory. "Borrowed" in this context refers to memory
     /// that will be cleaned up by some other scope and is not required to be freed
     /// by the caller. An example of this would be a `char *` pointer to kernel memory
     /// where the caller can access the memory but is not responsible for its
     /// allocation or deallocation.
     SafeBorrowedMemZero,
-    |self_: &mut SafeBorrowedMemZero| {
-        libcryptsetup_rs_sys::crypt_safe_memzero(self_.0, self_.1);
-    },
     /// Construct a safe memory handle from a pointer and a size.
     ///
     /// # Safety
     ///
     /// The length must match the length of the exposed memory block
     /// or memory corruption could occur.
+    from_ptr
 }
+#[cfg(cryptsetup23supported)]
+memzero!(SafeBorrowedMemZero);
+as_ref!(SafeBorrowedMemZero);
 
 /// Handle to allocated memory from libcryptsetup
 pub struct SafeMemHandle(*mut c_void, usize);
@@ -104,15 +134,6 @@ impl SafeMemHandle {
         let ptr = ptr_to_result!(unsafe { libcryptsetup_rs_sys::crypt_safe_alloc(size) })?;
         Ok(SafeMemHandle(ptr, size))
     }
-
-    /// Zero the data in the buffer. This is not necessary in most circumstances
-    /// unless the user wants to reuse the buffer and reinitialize with zeros.
-    /// This method is never needed before the value is dropped as `Drop` will
-    /// safely zero the memory for the user.
-    #[cfg(cryptsetup23supported)]
-    pub fn memzero(&mut self) {
-        unsafe { libcryptsetup_rs_sys::crypt_safe_memzero(self.0, self.1) };
-    }
 }
 
 impl Drop for SafeMemHandle {
@@ -120,18 +141,9 @@ impl Drop for SafeMemHandle {
         unsafe { libcryptsetup_rs_sys::crypt_safe_free(self.0) }
     }
 }
-
-impl AsRef<[u8]> for SafeMemHandle {
-    fn as_ref(&self) -> &[u8] {
-        unsafe { slice::from_raw_parts(self.0 as *const _ as *const u8, self.1) }
-    }
-}
-
-impl AsMut<[u8]> for SafeMemHandle {
-    fn as_mut(&mut self) -> &mut [u8] {
-        unsafe { slice::from_raw_parts_mut(self.0 as *mut u8, self.1) }
-    }
-}
+#[cfg(cryptsetup23supported)]
+memzero!(SafeMemHandle);
+as_ref!(SafeMemHandle);
 
 #[cfg(all(test, cryptsetup23supported))]
 mod test {
@@ -144,16 +156,15 @@ mod test {
         let mut handle = SafeMemHandle::alloc(32).unwrap();
         handle.as_mut().write_all(&[20; 32]).unwrap();
         assert_eq!(&[20; 32], handle.as_ref());
-        handle.memzero();
+        handle.safe_memzero();
         assert_eq!(&[0; 32], handle.as_ref());
     }
 
     #[test]
     fn test_memzero_borrowed() {
         let mut slice = [0u8; 32];
-        let mut borrowed_handle = unsafe {
-            SafeBorrowedMemZero::from_ptr(slice.as_mut_ptr() as *mut _, slice.len())
-        };
+        let mut borrowed_handle =
+            unsafe { SafeBorrowedMemZero::from_ptr(slice.as_mut_ptr() as *mut _, slice.len()) };
         borrowed_handle.as_mut().write_all(&[33; 32]).unwrap();
         assert_eq!(&[33; 32], borrowed_handle.as_ref());
         std::mem::drop(borrowed_handle);
