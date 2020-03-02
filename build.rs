2a1b4b5216504a23b84a4b46b0344aba1810d858
John Baublitz <jbaublitz@redhat.com>
2020-03-02 09:42:45 -0500

    PR revision

--- build.rs
+++ build.rs
@@ -15,11 +15,11 @@ fn main() {
         .atleast_version("2.2.0")
         .probe("libcryptsetup")
     {
-        Ok(l) => Version::parse(&l.version).expect("Could not parse version"),
+        Ok(l) => Version::parse(&l.version).unwrap(),
         Err(e) => panic!("Bindings require at least cryptsetup-2.2.0: {}", e),
     };
     for ver in SUPPORTED_VERSIONS.iter().take_while(|ver_string| {
-        let iter_version = Version::parse(ver_string).unwrap();
+        let iter_version = Version::parse(ver_string).expect("Could not parse version");
         version >= iter_version
     }) {
         println!(
