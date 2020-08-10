48a039029f18abadfa2f96e37a27cfd322d52cfd
mulhern <amulhern@redhat.com>
2020-08-10 17:07:43 -0400

    Minimize allows for auto-generated bindings
    Signed-off-by: mulhern <amulhern@redhat.com>

--- libcryptsetup-rs-sys/src/lib.rs
+++ libcryptsetup-rs-sys/src/lib.rs
@@ -1,6 +1,5 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(clippy::all)]
+#![allow(clippy::redundant_static_lifetimes)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
