ce672403a58bce463f2813e9ec82dba3a0595b21
John Baublitz <jbaublitz@redhat.com>
2025-05-15 12:46:15 -0400

    Add additional valid options to CryptVolumeKeyHandle::get

--- src/key.rs
+++ src/key.rs
@@ -2,7 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use std::os::raw::{c_int, c_uint};
+use std::{
+    os::raw::{c_int, c_uint},
+    ptr,
+};
 
 use crate::{device::CryptDevice, err::LibcryptErr};
 
@@ -20,18 +23,23 @@ impl<'a> CryptVolumeKeyHandle<'a> {
     /// size
     pub fn get(
         &mut self,
-        keyslot: c_uint,
+        keyslot: Option<c_uint>,
         volume_key: &mut [u8],
-        passphrase: &[u8],
+        passphrase: Option<&[u8]>,
     ) -> Result<(c_int, crate::size_t), LibcryptErr> {
         let mut volume_key_size_t = volume_key.len();
         errno_int_success!(mutex!(libcryptsetup_rs_sys::crypt_volume_key_get(
             self.reference.as_ptr(),
-            keyslot as c_int,
+            keyslot
+                .map(|i| i as c_int)
+                .unwrap_or(libcryptsetup_rs_sys::CRYPT_ANY_SLOT),
             to_mut_byte_ptr!(volume_key),
             &mut volume_key_size_t as *mut _,
-            to_byte_ptr!(passphrase),
-            passphrase.len(),
+            passphrase
+                .as_ref()
+                .map(|s| to_byte_ptr!(s))
+                .unwrap_or(ptr::null()),
+            passphrase.map(|p| p.len()).unwrap_or(0),
         )))
         .map(|i| (i, volume_key_size_t))
     }
